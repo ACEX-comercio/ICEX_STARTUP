1a49f20d4f7bcdd79d3cd10e48d720d9ab1525e7
Rodrigo <cherry199662@gmail.com>
2018-02-08 17:30:47 -0500

    Add Seed User

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = [
+            [
+                'name' => 'Admin',
+                'lastname'=>'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123456')
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
