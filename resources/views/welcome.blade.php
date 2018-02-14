86795fe9e3a2cccacf381bd68f03325fcb44cacb
tunime <tunime.1@gmail.com>
2018-02-14 17:05:14 -0500

    integrando el inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,348 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<!-- Altron - Multi-Purpose Landing Page Template design by DSAThemes (http://www.dsathemes.com) -->
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class=" js no-touch" lang="en"><!--<![endif]--><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 
-        <title>Laravel</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="author" content="DSAThemes">	
+		<meta name="description" content="Altron - Multi-Purpose Landing Page Template">
+		<meta name="keywords" content="Responsive, HTML5 template, DSAThemes, Multi-Purpose, One Page, Landing, Business, Creative, Corporate, Agency Template, Project">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				
+		<!-- SITE TITLE -->
+		<title>Altron - Multi-Purpose Landing Page Template</title>
+							
+		<!-- FAVICON AND TOUCH ICONS  -->
+		<link rel="shortcut icon" href="images/logo-ecex.png" type="image/x-icon">
+		<link rel="icon" href="images/logo-ecex.png" type="image/x-icon">
+		<link rel="apple-touch-icon" sizes="144x144" href="images/logo-ecex.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="images/logo-ecex.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="images/logo-ecex.png">
+		<link rel="apple-touch-icon" href="images/logo-ecex.png">
+				
+		<!-- BOOTSTRAP CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+				
+		<!-- FONT ICONS -->
+		<link href="css/font-awesome.css" rel="stylesheet">	
+		<link href="css/themify-icons.css" rel="stylesheet">
+				
+		<!-- GOOGLE FONTS -->
+		<link href="css/css.css" rel="stylesheet" type="text/css">
+				
+		<!-- PLUGINS STYLESHEET -->
+		<link href="css/owl.css" rel="stylesheet">
+		<link href="css/flexslider.css" rel="stylesheet">
+		<link href="css/prettyPhoto.css" rel="stylesheet">
+		
+		<!-- ON SCROLL ANIMATION -->
+		<link href="css/animate.css" rel="stylesheet">
+				
+		<!-- TEMPLATE CSS -->
+		<link href="css/base.css" rel="stylesheet"> 	
+		<link href="css/style.css" rel="stylesheet"> 	
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<!-- RESPONSIVE CSS -->
+		<link href="css/responsive.css" rel="stylesheet"> 
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+			
+	</head>
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+	<body>
+	
+	
+	
+		<!-- PRELOADER
+		============================================= -->		
+		<div class="animationload" style="display: none;">
+			<div class="loader" style="display: none;"></div>
+		</div>
+		
+		
+		
+		<!-- PAGE CONTENT
+		============================================= -->	
+		<div id="page" class="page-wrapper">
+		
+		
+		
+			<!-- HEADER 
+			============================================= -->
+			<header id="header" class="no-bg header">		
+				<div class="navbar navbar-fixed-top">	
+					<div class="container">
+					
+						
+						<!-- Navigation Bar -->
+						<div class="navbar-header">
+						
+							<!-- Responsive Menu Button -->
+							<button type="button" id="nav-toggle" class="navbar-toggle text-right" data-toggle="collapse" data-target="#navigation-menu">
+								<span class="sr-only">Toggle navigation</span> 
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							
+							<!-- LOGO IMAGE -->
+							<!-- Recommended sizes 150x22px; -->
+							<a class="navbar-brand logo-white" href="#intro-7"><img src="images/logo-icex.png" alt="logo" style="height: 30px;"></a>
+							<a class="navbar-brand logo-black" href="#intro-7"><img src="images/logo-icex.png" alt="logo" style="height: 30px;"></a>
+							
+						</div>	<!-- End Navigation Bar -->
+						
+						
+						<!-- Navigation Menu -->
+						<nav id="navigation-menu" class="collapse navbar-collapse">
+							<ul class="nav navbar-nav navbar-right editContent">													
+								
+								<li><a href="/">Inicio</a></li>															
+								<li><a href="/inteligencia">Inteligencia comercial</a></li>
+								<li><a href="/analisis">Analisis de mercado</a></li>	
+								<li><a href="#pricing">Planes y productos</a></li>	
+							</ul>
+						</nav>  <!-- End Navigation Menu -->
+						
+						
+					</div>	  <!-- End container -->
+				</div>	   <!-- End navbar fixed top  -->
+			</header>	<!-- END HEADER -->	
+			
+			
+			
+			<!-- INTRO-7
+			============================================= -->
+			<section id="intro-7" class="intro-section">
+				<div class="overlay division">
+					<div class="container">		
+						<div id="intro-7-content" class="row">
+							
+							
+							<!-- INTRO TEXT -->
+							<div class="col-md-10 col-md-offset-1 intro-txt text-center white-color">
+								
+								<!-- Title -->
+								<h2>La herramienta para venta y exportacion que estabas esperando</h2>	
 
-            .position-ref {
-                position: relative;
-            }
+								<!-- Button -->
+								<a class="btn btn-lg btn-tra m-top-10" href="#">Pruevalo Gratis</a>		
+									
+							</div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+																
+						</div>	 <!-- End Intro Content -->
+					</div>    <!-- End container -->
+				</div>	   <!-- End overlay -->
+			</section>	<!-- END INTRO-7 -->
+		
+		
+		
+			<!-- WELCOME
+			============================================= -->
+			<section id="welcome" class="wide-50 welcome-section division">
+				<div class="container">		
+					<div class="row">
+					
+					
+						<!-- WELCOME QUOTE -->
+						<div class="col-lg-4 welcome-quote text-right m-bottom-50">
+						
+							<!-- Quote -->
+							<h3>La herramienta mas rapida del mercado con inteligecia comercial</p>
+														
+						</div>
+						
+						
+						<!-- WELCOME TEXT -->
+						<div class="col-md-6 col-lg-4 welcome-txt m-bottom-50">
+							
+							<!-- Text -->
+							<div class="p-bottom-20">
+							
+								<p>Quisque magna ipsum, lobortis nulla consequat, consequat 
+                                    empus massa. Mauris placerat iaculis nisl vestibulum vestibulum 
+								   elit tincidunt euismod purus lobortis. Bibendum non diam 
+                                    tempus aliquet. Mauris porta imperdiet lacus, at ultricies nisi 
+								   porttitor luctus. Phasellus placerat massa nec faucibus 
+                                    tempus dolor, felis orci lacinia risus
+								</p>
+								
+								<p>Etiam scelerisque semper justo ut convallis. Cras tempor, non convallis tincidunt, est eros tincidunt ligula, 
+								   dictum magna eu ligula, lacus eros varius fringilla imperdiet sodales sapien porttitor mauris 
+								</p>
+								
+							</div>
+							
+							<!-- Slogan -->
+							<div class="slogan-txt b-top p-top-20 p-bottom-20">
+								<h4 class="h4-lg">We're team of developers and designers. We love to design and develop ultra modern templates</h4>							
+							</div>
+						
+						</div>
+						
+						
+						<!-- WELCOME IMAGE -->
+						<div class="col-md-6 col-lg-4 welcome-img m-bottom-50">
+							<img class="img-responsive" src="images/welcome.jpg" alt="welcome-image">														
+						</div>
+					
+					
+					</div>	  <!-- End row -->  
+				</div>	   <!-- End container -->
+			</section>	<!-- END WELCOME -->
+						
+			<!-- PRICING
+			============================================= -->
+			<section id="pricing" class="wide-50 pricing-section division">
+				<div class="container">
 
-            .content {
-                text-align: center;
-            }
+					
+					<!-- TITLEBAR -->				
+					<div class="row">
+						<div class="col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2 section-title-thin">		
+							<h4>Packages and Pricing</h4>	
+							<h3>Select the pricing plan that suits your needs. Upgrade, downgrade or cancel anytime</h3>														
+						</div>
+					</div>	
 
-            .title {
-                font-size: 84px;
-            }
+				
+					<!-- PRICING TABLES HOLDER -->
+					<div class="row pricing-row">
+					
+					
+						<!-- PRICE PLAN STANDARD -->
+						<div class="col-sm-4 col-md-4 m-bottom-50">
+							<div class="pricing-table text-center">		
+								
+								<!-- Table Header  -->
+								<h3>Standard</h3>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+								<!-- Plan Price  -->
+								<div class="price">
+									<sup>$</sup>
+									<span>39</span>	
+									<p>Per Month</p>
+								</div>
+																	
+								<!-- Plan Features  -->
+								<ul class="features">
+									<li>Up to10 Projects</li>
+									<li>10 GB of Storage</li>
+									<li>Up to 250 Users</li>
+									<li>10 mySQL Database</li>
+								</ul>
+										
+								<!-- Table Button  -->
+								<a href="#" class="btn btn-lg">Selececionar</a>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+							</div>
+						</div>
+						
+						
+						<!-- PRICE PLAN ADVANCED -->
+						<div class="col-sm-4 col-md-4 m-bottom-50">	
+							<div class="pricing-table text-center highlight">		
+								
+								<!-- Table Header  -->
+								<h3>Advanced</h3>
+								<div class="down-arrow"></div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+								<!-- Plan Price  -->
+								<div class="price">
+									<sup>$</sup>
+									<span>69</span>	
+									<p>Per Month</p>
+								</div>
+																	
+								<!-- Plan Features  -->
+								<ul class="features">
+									<li>Unlimited Projects</li>
+									<li>50 GB of Storage</li>
+									<li>Up to 1000 Users</li>
+									<li>10 mySQL Database</li>
+								</ul>
+										
+								<!-- Table Button  -->
+								<a href="#" class="btn btn-lg">SSelececionar</a>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+							</div>
+						</div>
+						
+						
+						<!-- PRICE PLAN ULTIMATE -->
+						<div class="col-sm-4 col-md-4 m-bottom-50">
+							<div class="pricing-table text-center">		
+								
+								<!-- Table Header  -->
+								<h3>Ultimate</h3>
+
+								<!-- Plan Price  -->
+								<div class="price">
+									<sup>$</sup>
+									<span>99</span>	
+									<p>Per Month</p>
+								</div>
+																	
+								<!-- Plan Features  -->
+								<ul class="features">
+								    <li>Unlimited Projects</li>
+								    <li>150 GB of Storage</li>
+									<li>Unlimited Users</li>
+									<li>10 mySQL Database</li>
+								</ul>
+										
+								<!-- Table Button  -->
+								<a href="#" class="btn btn-lg">Selececionar</a>
+
+							</div>
+						</div>
+						
+						
+					</div>	 <!-- END PRICING TABLES HOLDER -->
+					
+					
+					<!-- PRICING NOTICE MESSAGE  -->
+					<div class="row pricing-notice">				 	
+						<div class="col-md-12 text-center m-bottom-50">					
+							<p>*To explore <span>your best plan</span> or for a custom quote, please contact sales at <span>(985) 765-4321</span> </p>							
+						</div>							
+					</div>
+
+					
+				</div>    <!-- End container -->		
+			</section>	<!-- END PRICING -->
+			
         </div>
-    </body>
+
+			
+
+	
+		<!-- EXTERNAL SCRIPTS
+		============================================= -->	
+		<script src="js/jquery-2.js" type="text/javascript"></script>
+		<script src="js/bootstrap.js" type="text/javascript"></script>	
+		<script src="js/modernizr.js" type="text/javascript"></script>
+		<script src="js/jquery_009.js" type="text/javascript"></script>
+		<script src="js/retina.js" type="text/javascript"></script>	
+		<script src="js/jquery_005.js" type="text/javascript"></script>	
+		<script src="js/jquery.js"></script>
+		<script defer="defer" src="js/jquery_007.js"></script>
+		<script defer="defer" src="js/jquery_006.js"></script>		
+		<script src="js/owl.js" type="text/javascript"></script>
+		<script src="js/jquery_002.js" type="text/javascript"></script>
+		<script defer="defer" src="js/jquery_008.js" type="text/javascript"></script>
+		<script src="js/waypoints.js" type="text/javascript"></script>	
+		<script src="js/jquery_004.js"></script>
+		<script src="js/contact_form.js" type="text/javascript"></script>	
+		<script defer="defer" src="js/jquery_003.js" type="text/javascript"></script>
+		
+		<!-- Custom Script -->		
+		<script src="js/custom.js" type="text/javascript"></script>	
+        </body>
 </html>
