840fbc138ce33f64b414bf34a7542dc2d4e0ea14
tunime <tunime.1@gmail.com>
2018-02-22 10:50:00 -0500

    resolviendo diseño

--- app/Inteligencia.php
+++ app/Inteligencia.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Inteligencia extends Model
 {
     //
+    protected $table = "inteligencia";
+    public $timestamps =false;
+    //definimos la elaciones many to many muchos a muchos
+    public function paises(){
+        return $this -> hasMany('App\Pais');
+    }
 }
--- app/Pais.php
+++ app/Pais.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pais extends Model
 {
     //
+    protected $table = "paises";
+    public $timestamps =false;
+
+    //realizamos la relacion many ot many muchos a muchos
+
+    public function inteligencias(){
+        return $this -> hasMany('App\Inteligencia');
+    }
 }
